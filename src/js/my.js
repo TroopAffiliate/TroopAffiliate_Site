5d1cbfb184fba9c6c2999dd818d75b8afcae3abd
AndrewDuarte <duarte.andrew@gmail.com>
2017-12-31 00:23:15 +1100

    Included form validation

--- src/js/my.js
+++ src/js/my.js
@@ -1,5 +1,5 @@
 
-// Mailchip form foucs
+// ===== Mailchip form foucs =====
 $(function(){
     $('#mc-embedded-subscribe-form .mc-field-group .email').focusout(function(){
         var text_val = $(this).val();
@@ -11,7 +11,37 @@ $(function(){
     });
 });
 
-// Hovering tool tip
+// ===== Hide & show form validation error label =====
+$(function(){
+    $('#mc-embedded-subscribe-form .mc-field-group .email').focusout(function(){
+        var text_val = $(this).val();
+        if(text_val === ""){
+        $('#mce-EMAIL-error').addClass('hidden');
+        } else {
+            $('#mce-EMAIL-error').removeClass('hidden');
+        }
+    });
+});
+
+// ===== Form validation =====
+
+// Required email field
+$(function(){
+    $('#mc-embedded-subscribe-form').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            }
+        }
+    });
+});
+// Email pattern
+$.validator.methods.email = function( value, element ) {
+    return this.optional( element ) || /[a-z]+@[a-z]+\.[a-z]+/.test( value );
+  }
+
+// ===== Hovering tool tip =====
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
