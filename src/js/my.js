ce4bc98ac193108c33b2ae3a8f5e46969487c3b4
Suraj Shakya <surajshakya@Surajs-MacBook-Pro.local>
2018-01-10 18:44:50 +1100

    email validation

--- src/js/my.js
+++ src/js/my.js
@@ -1,11 +1,38 @@
 // Mailchip form foucs
 $(function(){
-    $('#mc-embedded-subscribe-form .mc-field-group .email').focusout(function(){
-        var text_val = $(this).val();
-        if(text_val === ""){
-            $(this).removeClass('has-value');
+   
+    function checkValidEmail(){
+        var eml = $('#mc-embedded-subscribe-form .mc-field-group .email');
+        var text_val = eml.val();
+        
+        var pattern = new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$");
+        if (pattern.test(eml.val())) {
+            eml.removeClass('hasError');
+            eml.next('label').text('Email Address');
         } else {
-            $(this).addClass('has-value');
+            eml.addClass('hasError');
+            if(text_val === ""){
+                eml.removeClass('has-value');
+            } else {
+                eml.addClass('has-value');
+                eml.next('label').text('Not a valid Email Address');
+            }
+            return false;
+        }
+    }
+    //email
+    $('#mc-embedded-subscribe-form .mc-field-group .email').blur(function () {
+        checkValidEmail();
+    });
+    $('#mc-embedded-subscribe-form .mc-field-group .email').focus(function () {
+        $(this).removeClass('hasError');
+        $(this).next('label').text('Email Address');
+    });
+    
+
+    $('form').submit( function(){
+        if( checkValidEmail() === false ){
+            return false;
         }
     });
 });
